957798a19d3288ff54dc598d0b02b864d056b2d7
Alex Wormuth <alexwormuth@Stephanies-MacBook-Pro.local>
2015-09-13 13:46:00 -0700

    stuff

--- frequency.go
+++ frequency.go
@@ -2,14 +2,38 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"google.golang.org/grpc"
-	// "fmt"
-	// "net/http"
-	// "time"
+	"time"
+	"fmt"
+	"math/rand"
 )
 
 func test(c *gin.Context) {
-	c.JSON(200, gin.H{"test": "hello, web users. first change! oh yeah"})
+	name := ""
+
+	s := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(s)
+    num := r.Intn(2)
+
+    switch num {
+    	case 0:
+	    	name = "mike"
+	    	break;
+	    case 1:
+	    	name = "justin"
+	    	break;
+    }
+
+	c.JSON(200, gin.H{"message of the day": name + " is a noob"})
+}
+
+func parseSound(c *gin.Context) {
+	r := c.Request
+
+    soundData := r.PostFormValue("sound_data")
+
+    fmt.Println(soundData)
+
+	c.JSON(200, gin.H{"sound_data": soundData})
 }
 
 func main() {
@@ -17,6 +41,7 @@ func main() {
 
 	//Routes
 	g.GET("/", test)
+	g.POST("/sound", parseSound)
 
 
 	g.Run(":1337")
